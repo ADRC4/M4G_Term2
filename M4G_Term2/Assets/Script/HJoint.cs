95070061c33ac5f620604b8360e11cb175929ef9
Mengyu Huang <mengyu.huang@ucl.ac.uk>
2018-02-01 11:49:17 +0000

    Add Unity File
    Add Unity File

---
+++ M4G_Term2/Assets/Script/HJoint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HJoint : MonoBehaviour
+{
+    private HingeJoint tempHinge;
+    bool hasJoint;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (collision.gameObject.GetComponent<Rigidbody>() != null && !hasJoint)
+            {
+                //gameObject.AddComponent<HingeJoint>();
+                //gameObject.GetComponent<HingeJoint>().connectedBody = collision.rigidbody;
+                tempHinge = collision.gameObject.AddComponent<HingeJoint>();
+                tempHinge.GetComponent<HingeJoint>().connectedBody = collision.rigidbody;
+                tempHinge.anchor = new Vector3(0.5f, 0.5f, -0.25f);
+                tempHinge.axis = new Vector3(0, 0, 90);
+                hasJoint = true;
+            }
+        }
+    }
+}
---
+++ M4G_Term2/Assets/Script/MovementController_PB.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController_PB : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public float tumblingDuration = 0.5f;
+
+    void Update()
+    {
+        var dir = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+            dir = Vector3.forward;
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            dir = Vector3.back;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            dir = Vector3.left;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            dir = Vector3.right;
+
+        if (dir != Vector3.zero && !isTumbling)
+        {
+            StartCoroutine(Tumble(dir));
+        }
+    }
+
+    bool isTumbling = false;
+    //int count = 0;
+
+    //private void clickCount(object sender, System.EventArgs e)
+    //{
+    //    KeyCode keyPress = new KeyCode();
+    //    keyPress.Click += key_Press;
+    //}
+
+    //protected void key_Press(object sender, EventArgs e)
+    //{
+    //    count++;
+    //}
+
+    IEnumerator Tumble(Vector3 direction)
+    {
+        isTumbling = true;
+
+        var rotAxis = Vector3.Cross(Vector3.up * 5, direction);
+        var pivot = (transform.position + Vector3.down * 0.1f) + direction * 0.1f;
+        var _pivot = (transform.position + Vector3.down * 0.5f) + direction * 0.5f;
+
+        var startRotation = transform.rotation;
+        var endRotation = Quaternion.AngleAxis(90.0f, rotAxis) * startRotation;
+
+        var startPosition = transform.position;
+        var endPosition = transform.position + direction;
+
+        var rotSpeed = 90.0f / tumblingDuration;
+        var t = 0.0f;
+
+        while (t < tumblingDuration)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    t += Time.deltaTime;
+                    transform.RotateAround(pivot, rotAxis, rotSpeed * Time.deltaTime);
+                }
+                else
+                {
+                    t += Time.deltaTime;
+                    transform.RotateAround(_pivot, rotAxis, rotSpeed * Time.deltaTime);
+                }
+            }
+            //t += Time.deltaTime;
+            //transform.RotateAround(pivot, rotAxis, rotSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        transform.rotation = endRotation;
+        transform.position = endPosition;
+
+        isTumbling = false;
+    }
+}
